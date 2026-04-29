0bfeba71813e3ed251f09fa5e194dfcf0357c294
Karl Pietrzak <karl@medplum.com>
2026-04-29 09:36:40 -0400

    s3 client resource leak fix

--- packages/data-warehouse/src/download.ts
+++ packages/data-warehouse/src/download.ts
@@ -134,5 +134,6 @@ export async function downloadParquetFiles(options: DownloadParquetOptions): Pro
     return rows.length;
   } finally {
     connection.closeSync();
+    s3Client.destroy();
   }
 }
